3c1c6ea84ab3c9d54d5fb5e32c272589e26899d1
Sam Budgett <sambudgett@hotmail.co.uk>
2021-06-27 15:57:20 +0100

    Finishing ReadMe and fixing some bugs in api

--- ReadMe.md
+++ ReadMe.md
@@ -13,7 +13,7 @@ Start server with:
 uvicorn api:app --reload
 ```
 
-Now navigate to http://127.0.0.1:8000/
+Now open http://127.0.0.1:8000/docs in browser
 
 ## API Interfaces
 The easiest way to explore the api is to go to 
@@ -21,8 +21,8 @@ http://127.0.0.1:8000/docs
 
 The follow sections outline the different interfaces and the curl commands to interact with them.
 
-
-### Model Training http://127.0.0.1:8000/train
+### POST Commands
+#### Model Training http://127.0.0.1:8000/train
 Interface to training model. provide:
  * list of features to train on. Suggested default of
 [ "time",  "ejection_fraction", "serum_creatinine"] 
@@ -31,21 +31,9 @@ Interface to training model. provide:
 * training and validation split
  
 
- 
 The following curl command will start training:
 ```bash
-curl -X 'POST' \
-  'http://127.0.0.1:8000/train' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "list_of_features": [
-    "time",
-    "ejection_fraction",
-    "serum_creatinine"
-  ],
-  "data_loc": "data/heart_failure_clinical_records_dataset.csv"
-}'
+curl -d "{ \"list_of_features\":[\"time\", \"ejection_fraction\", \"serum_creatinine\"],  \"data_loc\":\"data/heart_failure_clinical_records_dataset.csv\", \"training_split\":0.6}" -X "POST" "http://127.0.0.1:8000/train" -H "Content-Type:application/json"
 ```
 
 The API will respond with the statistics for the validation set during training
@@ -54,42 +42,68 @@ The API will respond with the statistics for the validation set during training
 * "area_under_curve": 0.9194365305476416,
 
 
-### Model Prediction http://127.0.0.1:8000/predict
+#### Model Prediction http://127.0.0.1:8000/predict
 Once trained the model can be used to predict the likelihood of heart failure from a set of features.
 
 Simply provide a list of values for the features which were trained on. 
 
-The following curl will post a job
+The following curl command will post a job:
  ```bash
-curl -X 'POST' \
-  'http://127.0.0.1:8000/predict' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "features": {
-    "time": 293,
-    "ejection_fraction": 100,
-    "serum_creatinine": 130
-  }
-}'
+curl -d "{ \"features\":{\"time\": 23, \"ejection_fraction\": 38, \"serum_creatinine\":130}}" -X "POST" "http://127.0.0.1:8000/predict" -H "Content-Type:application/json"
 ```
-
 This will return predictions for the ensemble and the individual predictions of each model
 
+
+#### Model Evaluation http://127.0.0.1:8000/evaluate
+This will repeat training a number of times and perform cross validation. Each run will perform a random training and 
+validation split and return the results. The number of runs is controlled by the repeats parameter and the curl command 
+is the following
+```bash
+curl -d "{ \"list_of_features\":[\"time\", \"ejection_fraction\", \"serum_creatinine\"],  \"data_loc\":\"data/heart_failure_clinical_records_dataset.csv\", \"training_split\":0.6, \"repeats\":10 }" -X "POST" "http://127.0.0.1:8000/evaluate" -H "Content-Type:application/json"
+```
+ 
+### GET commands
+#### Root http://127.0.0.1:8000/
+Will return json of all predictions that have been completed so far.
+
+#### Pair plots http://127.0.0.1:8000/visualise/pair
+Returns pair plot of feature visualisation. 
+Note this requires training to be done before hand as this is where data is loaded.
+
+#### Histogram plots http://127.0.0.1:8000/visualise/hist
+Returns histogram of feature visualisation. 
+Note this requires training to be done before hand as this is where data is loaded.
+
+#### History of predictions http://127.0.0.1:8000/predict/history
+Returns entire history of predictions 
+
+#### Current trained model RoC curve plots http://127.0.0.1:8000/train/roc_curve
+Returns roc curve of false positive rate vs true positive rate. Note this requires training to be done before hand.
+
+#### Overall Evaluation RoC curve 'http://127.0.0.1:8000/evaluate/roc_curve'
+Returns roc curve for evaluation if evaluation has been completed.
  
  ## ML architecture approach
 Building on the well established work on this small dataset such as in [1], this quick proof of concept work aims to 
 show that it is possible to train a large ensemble of simple machine learning classifiers on the e UCI Heart failure 
 clinical records Data Set. 
 
-Based of [1] ['time', 'ejection_fraction', 'serum_creatinine']
+Code from https://www.kaggle.com/andrewmvd/heart-failure-clinical-data/code inspired some of this work. In particular:
+
+* https://www.kaggle.com/nayansakhiya/heart-fail-analysis-and-quick-prediction
+* https://www.kaggle.com/sanchitakarmakar/heart-failure-prediction-visualization
+
+Based of [1] ['time', 'ejection_fraction', 'serum_creatinine'] were chosen as the three main features to use although 
+the interface and algorithm can use any set of features desired.
+
  
  ## Structure
  The repository contains the following files:
  * api.py: simple api to training algorithm visualising results and predicting on new data
  * data_utils.py: methods for loading and handling data
  * data_visualisation.py: methods for visualising data
- * ml_class.py: class for controlling 
+ * ml_class.py: class for controlling machine learning training, prediction and evaluation. 
+ Also contains some initial code to do hyper-paramter optimisation with optuna (although this is out of scope of this current work)
  
  ## References 
   [1]  Chicco, D., Jurman, G. Machine learning can predict survival of patients with
--- api.py
+++ api.py
@@ -6,13 +6,20 @@ from data_utils import read_data_features
 from ml_class import MachineLearningEnsemble
 from data_visualisation import visualise_data
 import numpy as np
-
+import os
+import copy
 
 class PredictRequest(BaseModel):
-    features: Dict[str, float] = {'time': 23, 'ejection_fraction': 38, 'serum_creatinine': 130}
+    """
+    Class for requesting prediction
+    """
+    features: Dict[str, float] = {'time': 23.0, 'ejection_fraction': 38.0, 'serum_creatinine': 130.0}
 
 
 class TrainRequest(BaseModel):
+    """
+    Class for training requests
+    """
     list_of_features: List[str] = ['time', 'ejection_fraction', 'serum_creatinine']
     data_loc: str = 'data/heart_failure_clinical_records_dataset.csv'
     training_split: float = 0.6
@@ -20,16 +27,22 @@ class TrainRequest(BaseModel):
 
 
 class TrainResponse(BaseModel):
+    """
+    Class for responding with stats about training success
+    """
     accuracy: Optional[float]
     f1_score: Optional[float]
     area_under_curve: Optional[float]
-    list_of_accuracies: Optional[Dict[str, float]]
+    matthews_correlation_coefficient_score: Optional[float]
+    component_accuracies_out: Optional[Dict[str, float]]
     error: Optional[str]
 
 
 class ModelResponse(BaseModel):
-    features: Dict[str, float]
-    predictions: Optional[List[Dict[str, float]]]
+    """
+    Class for returning detection information.
+    """
+    predictions: Optional[List[Dict[str, Dict[str, float]]]]
     error: Optional[str]
 
 
@@ -41,6 +54,10 @@ data = None
 
 @app.get("/", response_model=ModelResponse)
 async def root() -> ModelResponse:
+    """
+    returns all predictions
+    :return:
+    """
     return ModelResponse(predictions=all_predictions)
 
 
@@ -48,7 +65,7 @@ async def root() -> ModelResponse:
 async def visualise_pair_plot_api() -> FileResponse:
     if ml_model.model_trained_flag:
         visualise_data(ml_model.data_frame, list_of_plots=['pair'],
-                       list_of_features=ml_model.list_of_features,
+                       list_of_features=copy.deepcopy(ml_model.list_of_features),
                        label_name="DEATH_EVENT", save_location='test_data_visualisations/')
     return FileResponse('test_data_visualisations/pair_plot.png')
 
@@ -57,9 +74,14 @@ async def visualise_pair_plot_api() -> FileResponse:
 async def visualise_pair_plot_api() -> FileResponse:
     if ml_model.model_trained_flag:
         visualise_data(ml_model.data_frame, list_of_plots=['hist'],
-                       list_of_features=ml_model.list_of_features,
+                       list_of_features=copy.deepcopy(ml_model.list_of_features),
                        label_name="DEATH_EVENT", save_location='test_data_visualisations/')
     return FileResponse('test_data_visualisations/histograms.png')
+
+
+@app.get("/predict/history")
+async def get_model_predictions() -> ModelResponse:
+    return ModelResponse(predictions=all_predictions)
 
 
 @app.post("/predict")
@@ -77,7 +99,7 @@ async def get_model_predictions(request: PredictRequest) -> ModelResponse:
                 query_data.append(request.features[feature_name])
             else:
                 return ModelResponse(
-                    error=f'{feature_name} has not been found in input. '
+                    error=f'Feature: {feature_name} has not been found in input. '
                     f'Please provide values for the full feature list used in training: {ml_model.list_of_features}'
                 )
         query_data = np.expand_dims(np.asarray(query_data), axis=0)
@@ -89,7 +111,9 @@ async def get_model_predictions(request: PredictRequest) -> ModelResponse:
                            'k-nn': predictions[6]}
         all_predictions.append({"features": request.features,
                                 "predictions": predictions_out})
-        return ModelResponse(predictions=all_predictions)
+        current_predictions = [{"features": request.features,
+                                "predictions": predictions_out}]
+        return ModelResponse(predictions=current_predictions)
 
     else:
         return ModelResponse(
@@ -107,8 +131,26 @@ async def train_model(request: TrainRequest) -> TrainResponse:
     area_under_curve, accuracy, f1_score, mcc_score, fpr, tpr, component_accuracies, component_f1_scores, \
         component_mcc_scores = ml_model.initialise_model_from_data_file(request.list_of_features, request.data_loc,
                                                                         training_split=request.training_split)
+    component_accuracies_out = {'Combined_accuracy': accuracy, 'non-linear-svm': component_accuracies[0],
+                                'decision-tree': component_accuracies[1], 'random-forest': component_accuracies[2],
+                                'ada-boost': component_accuracies[3], 'mlp': component_accuracies[4],
+                                'log_reg': component_accuracies[5], 'k-nn': component_accuracies[6]}
 
-    return TrainResponse(accuracy=accuracy, f1_score=f1_score, area_under_curve=area_under_curve)
+    return TrainResponse(accuracy=accuracy, f1_score=f1_score, area_under_curve=area_under_curve,
+                         matthews_correlation_coefficient_score=mcc_score,
+                         component_accuracies_out=component_accuracies_out)
+
+
+@app.get("/train/roc_curve")
+async def get_overall_roc_curve() -> FileResponse or str:
+    """
+    gets roc curve for overall training
+    :return: FileResponse: roc curve of from current training option
+    """
+    if os.path.isfile('results/current_roc_curve.png'):
+        return FileResponse('results/current_roc_curve.png')
+    else:
+        return "Overall Evaluation not run yet, please run http://127.0.0.1:8000/train"
 
 
 @app.post("/evaluate")
@@ -116,7 +158,7 @@ async def evaluate_model(request: TrainRequest) -> TrainResponse:
     """
     Trains models with data from path
     :param request: features to use and data location
-    :return:
+    :return: TrainResponse: training information
     """
     test_input_data = read_data_features(request.list_of_features, data_path=request.data_loc)[0].T
     test_target = np.squeeze(read_data_features(["DEATH_EVENT"])[0])
@@ -124,5 +166,17 @@ async def evaluate_model(request: TrainRequest) -> TrainResponse:
         = ml_model.average_results(test_input_data, test_target, repeats=request.repeats,
                                    plot_save_loc='./results/overall_roc_curve.png')
     return TrainResponse(accuracy=np.mean(list_accuracy), f1_score=np.mean(list_f1_score),
-                         area_under_curve=np.mean(list_area_under_curve))
+                         area_under_curve=np.mean(list_area_under_curve),
+                         matthews_correlation_coefficient_score=np.mean(list_mcc_score))
 
+
+@app.get("/evaluate/roc_curve")
+async def get_overall_roc_curve() -> FileResponse or str:
+    """
+    gets roc curve for overall training
+    :return: FileResponse: roc curve of from all cross validation
+    """
+    if os.path.isfile('./results/overall_roc_curve.png'):
+        return FileResponse('./results/overall_roc_curve.png')
+    else:
+        return "Overall Evaluation not run yet, please run http://127.0.0.1:8000/evaluate"
